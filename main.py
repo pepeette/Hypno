7e886bc76b9bd056908b49be30a5752591774fb0
pepeette <59664130+pepeette@users.noreply.github.com>
2024-08-27 08:30:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ with tgb.Page() as root_page:
         tgb.text("   ")
         tgb.text("   ")
         #with tgb.layout("1 2"):
-        tgb.button("Act Now: FIRST SESSION FREE ", on_action=open_calendly, style="display: flex; align-items: center; justify-content: center;")
+        tgb.button("IS IT FOR ME? FREE ASSESSMENT", on_action=open_calendly, style="display: flex; align-items: center; justify-content: center;")
         tgb.text("Transform your life with my hypnotherapy sessions, designed to create lasting behavior change by integrating the powerful tools of DBT (Dialectical Behavior Therapy).")# Unlock your full potential and achieve enduring results through a unique blend of therapeutic techniques.", style="color: white; text-align: center;")
         tgb.text("   ")
         tgb.text("   ")
@@ -208,7 +208,8 @@ with tgb.Page() as page2:
         with tgb.part():
             tgb.text("#### Find Us Here", mode="md", style="text-align: center;")
             # Embed Google Maps (Replace the URL with your actual Google Maps link)
-            tgb.text("You can find us at: [46/9 Soi Sukhumvit 49, Klong Ton Nua, Wattana District](https://www.google.com/maps/search/?api=1&query=46/9+Soi+Sukhumvit+49,+Klong+Ton+Nua,+Wattana+District)", mode='md')
+            tgb.text("You can find us at: [46/9 Soi Sukhumvit 49, Klong Ton Nua, Wattana District](https://www.google.com/maps/place/the+Hive+Thonglor/@13.7320825,100.571723,17z/data=!3m1!4b1!4m6!3m5!1s0x30e29e55a95f6f93:0xf9a8634f35bf33a6!8m2!3d13.7320825!4d100.5765939!16s%2Fg%2F1q6b9rc2_?entry=ttu&g_ep=EgoyMDI0MDgyMS4wIKXMDSoASAFQAw%3D%3D)", mode='md')
+            tgb.image("./img/Map.png", width="100%")
 
         # Right Column: Get in Touch Form
         with tgb.part():
