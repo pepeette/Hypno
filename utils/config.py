9e3e6388d1161b2ec9d37616f4d3e1d0aaac001c
pepeette <59664130+pepeette@users.noreply.github.com>
2025-09-05 20:01:34 +0700

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -7,8 +7,8 @@ class AppConstants:
     """Application-wide constants"""
     
     # Navigation
-    NAVIGATION_OPTIONS = ["Hypnosis", "Method", "Testimonials", "Blog", "Book Now"]
-    NAVIGATION_ICONS = ["house", "gear", "star", "book", "calendar"]
+    NAVIGATION_OPTIONS = ["Hypnosis", "Method", "Blog", "Testimonials", "Book Now"]
+    NAVIGATION_ICONS = ["house", "gear", "book", "star", "calendar"]
     
     # Contact information
     CONTACT_INFO = {
