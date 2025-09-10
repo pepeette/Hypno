b0ca0e8f00d4824f97301ea1a4dc248f31cf74bf
pepeette <59664130+pepeette@users.noreply.github.com>
2025-09-10 15:11:07 +0700

    Update assess.py

--- pages/assess.py
+++ pages/assess.py
@@ -2130,10 +2130,7 @@ class AdaptiveBehavioralAssessment:
                     self._advance_question()
                     st.rerun()
             else:
-                if st.button("Skip", key=f"q_{q_id}_skip", use_container_width=True):
-                    self._save_response(q_id, "Skipped", {"patterns": None, "weights": [0]})
-                    self._advance_question()
-                    st.rerun()
+                continue
 
     def _render_navigation(self, current_q_id):
         col1, col2, col3 = st.columns([1, 2, 1])
